c69440da7837558da21477e1828b30f01693adf7
mostafa hk <mostafa.hk@gmail.com>
2021-08-30 12:35:38 +0430

    ESP8266, ESP32 compatibility

--- src/RS485.h
+++ src/RS485.h
@@ -26,7 +26,7 @@
 #ifdef PIN_SERIAL1_TX
 #define RS485_DEFAULT_TX_PIN PIN_SERIAL1_TX
 #else
-#define RS485_DEFAULT_TX_PIN 1 
+#define RS485_DEFAULT_TX_PIN 1
 #endif
 #endif
 
@@ -39,11 +39,21 @@
 #elif ARDUINO_SAMD_ZERO
 #define RS485_DEFAULT_DE_PIN A4
 #define RS485_DEFAULT_RE_PIN A5
+#elif defined(ESP32) || defined(ESP8266)
+#define RS485_DEFAULT_DE_PIN 0
+#define RS485_DEFAULT_RE_PIN 0
 #else
 #ifndef RS485_DEFAULT_DE_PIN
 #define RS485_DEFAULT_DE_PIN A6
 #define RS485_DEFAULT_RE_PIN A5
 #endif
+
+#if defined(ESP8266)
+#define RS485_SER_CONF_TYPE SerialConfig
+#elif defined(ESP32)
+#define RS485_SER_CONF_TYPE uint32_t
+#else
+#define RS485_SER_CONF_TYPE uint16_t
 #endif
 
 
@@ -58,9 +68,9 @@ class RS485Class : public Stream {
     RS485Class(HardwareSerial& hwSerial, int txPin, int dePin, int rePin);
 
     virtual void begin(unsigned long baudrate);
-    virtual void begin(unsigned long baudrate, uint16_t config);
+    virtual void begin(unsigned long baudrate, RS485_SER_CONF_TYPE config);
     virtual void begin(unsigned long baudrate, int predelay, int postdelay);
-    virtual void begin(unsigned long baudrate, uint16_t config, int predelay, int postdelay);
+    virtual void begin(unsigned long baudrate, RS485_SER_CONF_TYPE config, int predelay, int postdelay);
     virtual void end();
     virtual int available();
     virtual int peek();
